f5c8597747044b5e131fe2b5f700699413ced43c
Jasmine Guy <jasfromnz@gmail.com>
2021-06-13 16:20:45 -0500

    updates user attribute in post schema

--- models/posts.js
+++ models/posts.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    uid: String,
+    user: Object,
     title: String,
     link: String,
     rating: Number,
