d8a88e1fb1dfae895c075483db2ef33591d4cc19
Jasmine Guy <jasfromnz@gmail.com>
2021-06-13 16:22:50 -0500

    updates final schema setup

--- models/posts.js
+++ models/posts.js
@@ -9,4 +9,4 @@ const postSchema = new Schema({
     notes: String
 }, { timestamps: true });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Post', postSchema);gi
